6a17fa3d474f3764d99e5f49d8c7012db17c3523
nopeless <38830903+nopeless@users.noreply.github.com>
2021-08-20 21:52:27 +0900

    add null check and support arrays

--- src/util/overwriteObject.js
+++ src/util/overwriteObject.js
@@ -2,7 +2,6 @@
 
 function overwriteObject (a, b) {
   // Write b on top of a
-
   // Only handle with deletes
   for (const key in a) {
     // eslint-disable-next-line no-prototype-builtins
@@ -10,11 +9,31 @@ function overwriteObject (a, b) {
       delete a[key];
     }
   }
+
+  // Some useful checks
+  if (Array.isArray(a) && !Array.isArray(b) || !Array.isArray(a) && Array.isArray(b)) {
+    throw new Error(`${a} is not compatible with ${b}`);
+  }
+
+  // If a is an Array, update by indices
+  if (Array.isArray(a) && Array.isArray(b)) {
+    // Delete the keys
+    for (const k in a) {
+      delete a[k];
+    }
+    const keys = Object.keys(b);
+    for (let i = 0; i < keys.length; i++) {
+      if (b[i] !== undefined) a[i] = b[i];
+    }
+    a.length = keys.length;
+    return;
+  }
+
   // Handle modifies
   for (const key in b) {
     // eslint-disable-next-line no-prototype-builtins
     if (b.hasOwnProperty(key)) {
-      if (typeof a[key] == `object`) {
+      if (typeof a[key] === `object` && a[key] !== null) {
         // Do not update the reference
         overwriteObject(a[key], b[key]);
       } else {
