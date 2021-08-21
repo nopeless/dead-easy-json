4b63ac1e485a9a2e0952834c561dc5f4a58fb84b
nopeless <38830903+nopeless@users.noreply.github.com>
2021-08-21 11:32:42 +0900

    ignore single letter js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     "ecmaVersion": 2020
   },
   // ignore certain files
-  "ignorePatterns": [`docs/**/*.js`],
+  "ignorePatterns": [`docs/**/*.js`, `?.js`],
   "extends": `eslint:recommended`,
   "rules": {
     "quotes": [1, `backtick`],
